01870477b639b93bd80d243ef690380c50d07a53
nattapong18-en <664110310060@npu.ac.th>
2026-07-09 05:45:35 +0700

    first commit

---
+++ build/CMakeFiles/my_program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_program.dir/link.d"
+  "CMakeFiles/my_program.dir/main.cpp.o"
+  "CMakeFiles/my_program.dir/main.cpp.o.d"
+  "my_program"
+  "my_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
